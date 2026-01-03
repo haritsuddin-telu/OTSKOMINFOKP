88ec8a85437a3989c7a9b6486640c508e2daa3f4
unknown <haritsuddin88@gmail.com>
2026-01-03 23:51:59 +0700

    Optimasi WhatsApp Engine (Auto)

--- whatsapp-service/index.js
+++ whatsapp-service/index.js
@@ -17,7 +17,16 @@ let clientStatus = 'INITIALIZING'; // INITIALIZING, WAITING_FOR_QR, READY, AUTHE
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--disable-gpu'
+        ]
     }
 });
 
